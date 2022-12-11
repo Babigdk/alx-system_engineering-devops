dc7649b5738e1b3c6bd1fbe8d3a448eb0fb19b20
Babigdk <Babigdk@gmail.com>
2022-12-10 16:05:04 -0800

    configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # Install the puppet-lint package
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
