df2c3ce030e9b23bc29385299de521720cfd25ae
Babigdk <Babigdk@gmail.com>
2022-12-15 03:32:00 +0000

    set config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,14 @@
-# set up your client SSH configuration file so that you can connect to a server without typing a password.
-file_line { 'Turn off passwd auth':
+# Make passwordAuthentication and add new private key
+file_line { 'replace passwordAuthentication':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  replace => true,
+  line    => 'PasswordAuthentication no',
+  match   => 'PasswordAuthentication yes',
+}
+
+file_line { 'add the private key ~/.ssh/holberton':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-  }
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/holberton',
-  }
+  line   => 'IdentityFile ~/.ssh/holberton',
+}
